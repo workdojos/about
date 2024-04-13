c164b4560ec4e2e599f96401d1ce2e83bc844a9a
workdojos <itsarectangle@gmail.com>
2024-04-13 13:06:08 -0500

    asd

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -58,7 +58,7 @@ const HeroSection = () => {
           <div className="lg:w-[400px] lg:h-[300px] relative ">
             <Image
               style={{borderRadius: 20}}
-              src="/images/tv3.gif"
+              src="/images/tv5.gif"
               alt="A vintage tv with an old movie"
               className=".img-container"
               width={307}
