d09f083d723b8bd6370c7ff529b3fecf215399fd
workdojos <itsarectangle@gmail.com>
2024-04-13 17:27:05 -0500

    sd

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -58,7 +58,7 @@ const HeroSection = () => {
           <div className="lg:w-[400px] lg:h-[300px] relative ">
             <Image
               style={{borderRadius: 20}}
-              src="/images/transtv.mp4"
+              src="/images/dojofans.gif"
               className=".img-container"
               width={480}
               height={480}
