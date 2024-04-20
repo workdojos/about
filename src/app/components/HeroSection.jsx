bb478f6a1aa8471b9fe82f5248913170ff6290f6
workdojos <itsarectangle@gmail.com>
2024-04-20 14:17:08 -0500

    as

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -58,7 +58,7 @@ const HeroSection = () => {
           <div className="lg:w-[400px] lg:h-[300px] relative ">
             <Image
               style={{borderRadius: 20}}
-              src="/images/flick.gif"
+              src="/images/drive.webp"
               className=".img-container"
               width={480}
               height={480}
