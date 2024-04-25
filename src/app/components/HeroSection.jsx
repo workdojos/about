4ac3db5142f83a183f1a7f31cf2c0f71623ef24e
workdojos <itsarectangle@gmail.com>
2024-04-25 12:38:39 -0500

    df

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -59,7 +59,7 @@ const HeroSection = () => {
           <div className="lg:w-[400px] lg:h-[300px] relative ">
             <Image
               style={{borderRadius: 20}}
-              src="/images/open2.png"
+              src="/images/open3.png"
               className=".img-container"
               width={480}
               height={480}
