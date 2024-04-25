8eeec51299f23f4801681d757aa7d0e691d3aeec
workdojos <itsarectangle@gmail.com>
2024-04-25 14:03:17 -0500

    as

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -108,7 +108,7 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
         <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image style={{ borderRadius: 16}}src="/images/cool.jpg" width={1800} height={1200}   />
+        <Image style={{ borderRadius: 16}}src="/images/opensource.jpg" width={500} height={875}   />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">Hire Us</h2>
 
