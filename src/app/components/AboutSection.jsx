c5adfdde22dd3e961f79291157622c445fe4323d
workdojos <itsarectangle@gmail.com>
2024-05-07 06:45:53 -0500

    sd

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -3,6 +3,7 @@ import React, { useTransition, useState } from "react";
 import Image from "next/image";
 import TabButton from "./TabButton";
 
+
 const TAB_DATA = [
   {
     title: "History",
@@ -101,9 +102,15 @@ const AboutSection = () => {
 
   return (
     <section className="text-white" id="about">
-        <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image style={{ borderRadius: 16}}src="/images/sunsun.gif" width={1280} height={720}   />
-        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <div className="lg:w-[786px] lg:h-[443px] relative ">
+            <Image
+              style={{borderRadius: 20}}
+              src="/images/sunsun.gif"
+              className=".img-container"
+              width={480}
+              height={480}
+            />
+          </div>
           <h2 className="text-4xl font-bold text-white mb-4">Open-Source Cool</h2>
 
 
